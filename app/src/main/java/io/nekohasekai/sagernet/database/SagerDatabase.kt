21d92cd765fed05a6c44ccb8495439c4d12a4598
karriiiii <www.pro100mihail@gmail.com>
2026-05-16 23:10:16 +0300

    Update SagerDatabase.kt

--- app/src/main/java/io/nekohasekai/sagernet/database/SagerDatabase.kt
+++ app/src/main/java/io/nekohasekai/sagernet/database/SagerDatabase.kt
@@ -46,19 +46,44 @@ abstract class SagerDatabase : RoomDatabase() {
                     override fun onCreate(db: SupportSQLiteDatabase) {
                         super.onCreate(db)
                         
-                        // Твой кастомный JSON для поля config
-                        val customConfig = "{\"rules\":[{\"domain\":[\"max.ru\",\"oneme.ru\",\"api.oneme.ru\"],\"geosite\":[\"category-ads-all\"],\"outbound\":\"block\"},{\"domain\":[\"telegram.org\",\"t.me\"],\"geosite\":[\"telegram\"],\"outbound\":\"proxy\"},{\"domain_keyword\":[\"yandex\",\"yastatic\",\"yadi.sk\",\"xn--80aswg\",\"xn--d1acpjx3f.xn--p1ai\",\"xn--c1avg\",\"xn--80asehdb\",\"xn--p1acf\",\"xn--p1ai\",\"gstatic.com\",\"tineye\",\"vk.com\",\"userapi.com\",\"vk-cdn.me\",\"mvk.com\",\"vk-cdn.net\",\"vk-portal.net\",\"vk.cc\",\"tradingview\"],\"domain_suffix\":[\".ru\",\".su\",\".by\"],\"geoip\":[\"private\",\"ru\",\"by\"],\"geosite\":[\"vk\",\"yandex\"],\"outbound\":\"direct\"}]}"
+                        // Формируем списки доменов и IP точно так же, как их вводил бы пользователь в UI (через перенос строки)
+                        val domainsAds = "geosite:category-ads-all\nmax.ru\noneme.ru\napi.oneme.ru"
                         
-                        // Вставляем запись в таблицу rules
+                        val domainsTg = "geosite:telegram\ntelegram.org\nt.me"
+                        
+                        val domainsRu = "geosite:vk\ngeosite:yandex\nkeyword:yandex\nkeyword:yastatic\nkeyword:yadi.sk\nkeyword:xn--80aswg\nkeyword:xn--d1acpjx3f.xn--p1ai\nkeyword:xn--c1avg\nkeyword:xn--80asehdb\nkeyword:xn--p1acf\nkeyword:xn--p1ai\nkeyword:gstatic.com\nkeyword:tineye\nkeyword:vk.com\nkeyword:userapi.com\nkeyword:vk-cdn.me\nkeyword:mvk.com\nkeyword:vk-cdn.net\nkeyword:vk-portal.net\nkeyword:vk.cc\nkeyword:tradingview\ndomain:ru\ndomain:su\ndomain:by"
+                        
+                        val ipsRu = "geoip:private\ngeoip:ru\ngeoip:by"
+
+                        // 1. Правило: Блокировка рекламы (outbound = -2)
+                        // userOrder = 0 (Самый высокий приоритет, срабатывает первым)
                         db.execSQL("""
                             INSERT INTO rules 
                             (name, config, userOrder, enabled, domains, ip, port, sourcePort, network, source, protocol, outbound, packages) 
                             VALUES 
-                            ('Обход RU (По умолчанию)', '$customConfig', 0, 1, '', '', '', '', '', '', '', 0, '')
+                            ('Блокировка рекламы', '', 0, 1, '$domainsAds', '', '', '', '', '', '', -2, '')
+                        """)
+
+                        // 2. Правило: Telegram через прокси (outbound = 0)
+                        // userOrder = 1
+                        db.execSQL("""
+                            INSERT INTO rules 
+                            (name, config, userOrder, enabled, domains, ip, port, sourcePort, network, source, protocol, outbound, packages) 
+                            VALUES 
+                            ('Telegram (Proxy)', '', 1, 1, '$domainsTg', '', '', '', '', '', '', 0, '')
+                        """)
+
+                        // 3. Правило: RU Сегмент напрямую (outbound = -1)
+                        // userOrder = 2
+                        db.execSQL("""
+                            INSERT INTO rules 
+                            (name, config, userOrder, enabled, domains, ip, port, sourcePort, network, source, protocol, outbound, packages) 
+                            VALUES 
+                            ('RU Сегмент (Direct)', '', 2, 1, '$domainsRu', '$ipsRu', '', '', '', '', '', -1, '')
                         """)
                     }
                 })
-                // --- КОНЕЦ ДОБАВЛЕНИЯ ---
+                // --- КОНЕЦ ДОБАВЛЕНИЯ ----
                 
                 .build()
         }
